9122a0447fbe872dfc68966fa7236a488c95143e
shubhampagare23 <shubhampagare0910@gmail.com>
2025-04-10 23:32:42 +0530

    Commiting the latest changes

---
+++ TestArtifactId/src/main/java/testMavenPackage/DBConnection.java
@@ -0,0 +1,63 @@
+package testMavenPackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
+
+//import codePractice.oracle;
+
+public class DBConnection {
+
+	public static void main(String a[]) {
+
+//		String url = "jdbc:oracle:thin:@localhost:1521:xe"; // Database details
+//
+//		// Username and password to access DB
+//		String user = "system";
+//		String pass = "12345";
+//
+//		// Entering the data
+//		Scanner k = new Scanner(System.in);
+//		System.out.println("Enter name:");
+//		String name = k.next();
+//		System.out.println("Enter roll no:");
+//		int roll = k.nextInt();
+//		System.out.println("Enter class:");
+//		String cls = k.next();
+//
+//		// Inserting data using SQL query
+//		String sql = "insert into student1 values('" + name + "'," + roll + ",'" + cls + "')";
+//
+//		// Connection class object
+//		Connection con = null;
+//
+//		try {
+//			// Registering drivers
+//			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+//
+//			// Establish connection
+//			con = DriverManager.getConnection(url, user, pass);
+//
+//			// Create a statement
+//			Statement st = con.createStatement();
+//
+//			// Execute the query
+//			int m = st.executeUpdate(sql);
+//			if (m == 1)
+//				System.out.println("Inserted successfully: " + sql);
+//			else
+//				System.out.println("Insertion failed");
+//
+//			// Close the connection
+//			con.close();
+//		} catch (Exception ex) {
+//			// Handle exceptions
+//			System.err.println(ex);
+//		}
+//
+//	}
+
+}
+}
+	
